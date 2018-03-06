0c6bcb33c22ce836907367a1becc8eee153431e7
袁杨 <yuanyang@uvoplus.com>
2018-03-06 11:35:59 +0800

    aaa ffff rebase2

--- commit.js
+++ commit.js
@@ -1,4 +1,11 @@
 var a = {};
 var fun = function() {console.log('commit')}
-
+<<<<<<< HEAD
+xx
+rebase2
+x
+=======
+xxx
+rebase -i
+>>>>>>> 7d9e0ef... 1. aaa rebase -i
 var fn = function() {console.log('changed')}
